703c127c980bb0082d93a74e9bdebc889e8dc720
yarzou <yarzou@gmail.com>
2018-07-26 17:52:09 +0200

    update feedBackRestController

--- src/main/java/fr/gtm/project/proxibanque/web/FeedBackRestController.java
+++ src/main/java/fr/gtm/project/proxibanque/web/FeedBackRestController.java
@@ -1,10 +1,17 @@
 package fr.gtm.project.proxibanque.web;
 
+import java.io.IOException;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,23 +26,23 @@ public class FeedBackRestController {
 	@Autowired
 	private FeedbackService FBservice;
 
-	@RequestMapping("/create/{id}")
+	@PostMapping("/{id}")
 	public FeedBack create(@PathVariable final FeedBack feedBack) {
 		return this.FBservice.create(feedBack);
 	}
 
-	@RequestMapping("/delete/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
 	public void delete(@PathVariable final Integer id) {
 		this.FBservice.delete(id);
 	}
 
-	@RequestMapping("/list")
+	@RequestMapping("/")
 	public List<FeedBack> list() {
 		return this.FBservice.list();
 	}
 
-	@RequestMapping("/read/{id}")
+	@RequestMapping("/{id}")
 	public FeedBack read(@PathVariable final Integer id) {
 		if (this.FBservice.read(id).isPresent()) {
 			return this.FBservice.read(id).get();
@@ -43,8 +50,12 @@ public class FeedBackRestController {
 		return null;
 	}
 
-	@RequestMapping("/update/{id}")
-	public FeedBack update(@PathVariable final FeedBack feedBack) {
+	@PutMapping("/update/{id}")
+	public FeedBack update(@RequestBody final FeedBack feedBack, final HttpServletResponse response)
+			throws IOException {
+		if (feedBack.getId() == null) {
+			response.sendError(HttpStatus.BAD_REQUEST.value(), "The feedBack id is missing to update.");
+		}
 		return this.FBservice.update(feedBack);
 	}
 
