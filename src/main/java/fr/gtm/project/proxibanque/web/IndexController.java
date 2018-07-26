37acdec2bdad048ca1c364897042caef69aec31a
yarzou <yarzou@gmail.com>
2018-07-26 15:45:21 +0200

    création du formulaire jsp pour Sondage

--- src/main/java/fr/gtm/project/proxibanque/web/IndexController.java
+++ src/main/java/fr/gtm/project/proxibanque/web/IndexController.java
@@ -1,6 +1,7 @@
 package fr.gtm.project.proxibanque.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,6 +10,12 @@ public class IndexController {
 
 	@RequestMapping({ "/", "/index" })
 	public ModelAndView index() {
+		final ModelAndView mav = new ModelAndView("index");
+		return mav;
+	}
+	
+	@GetMapping({"/", "/index"})
+	public ModelAndView updateArticle() {
 		final ModelAndView mav = new ModelAndView("index");
 		return mav;
 	}
